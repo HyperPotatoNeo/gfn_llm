a9192c8ddba41fc4dd7f254f0eecdc881fb5f91c
Johan Samir Obando Ceron <jobando0730@gmail.com>
2024-11-04 20:27:59 -0500

    .sh file for running experiments with RLOO and GSM8K dataset.

--- run_reasoning_task.sh
+++ run_reasoning_task.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 #SBATCH --time=100:00:00
-#SBATCH --mem=64G
+#SBATCH --mem=80G
 #SBATCH --partition lab-bengioy
 #SBATCH --gres=gpu:a100l:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mail-user=jobando0730@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --array=1
+
+module unload anaconda
+echo "loading modules"
+module load python/3.10 cudatoolkit/12.3.2
+
+echo "loading env"
+cd $HOME/johan_phd/
+source llm_gfn_git/bin/activate
+
+echo "running script.."
+cd $HOME/scratch/gfn_llm/
 
 python3 examples/scripts/rloo/rloo_GSM8K.py \
     --learning_rate 3e-6 \
@@ -16,7 +27,5 @@ python3 examples/scripts/rloo/rloo_GSM8K.py \
     --total_episodes 30000 \
     --model_name_or_path microsoft/rho-math-1b-v0.1 \
     --sft_model_path realtreetune/rho-1b-sft-GSM8K \
-    --non_eos_penalty \
     --stop_token eos \
-    --response_length 53 \
-    --sanity_check
+    --response_length 1024
